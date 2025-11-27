a267ffd1e38be15076c267d61509fe216d4131c8
MikeKomari <michaelk.projects@gmail.com>
2025-11-27 12:28:40 +0700

    added tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
   <!-- <link rel="stylesheet" href="{{ asset('bootstrap5.3/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="../../css/index.css"> -->
   
-  <!-- Kalau Tailwind -->
+  <!-- Kalau Tailwind  -->
   @vite('resources/css/app.css')
 </head>
 <body>
