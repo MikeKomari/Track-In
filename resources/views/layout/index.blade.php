8c00b8c2326842c08494ed591e129c7571a46a5e
Specticall <josephyusmita@gmail.com>
2025-11-27 17:04:18 +0700

    feat: navbar

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -6,16 +6,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
 
-        <link rel="stylesheet" href="{{ asset("bootstrap5.3/css/bootstrap.min.css") }}">
-        <link rel="stylesheet" href="../../css/index.css">
         @vite(["resources/css/app.css", "resources/js/app.js"])
         <script defer src="https://code.iconify.design/iconify-icon/2.0.0/iconify-icon.min.js"></script>
     </head>
 
     <body>
-        <main class="bg-background grid grid-cols-[15rem_1fr] min-h-dvh">
+        <main data-navbar-state='open'
+            class="transition-[grid-template-columns] duration-200 layout bg-background grid grid-cols-[3.85rem_1fr] min-h-dvh data-[navbar-state=open]:grid-cols-[15rem_1fr] group">
             @include("layout.navbar")
-            <section class="bg-background p-2">
+            <section class="bg-background p-2 pl-0">
                 <div class="bg-white h-full w-full rounded-xl">
                     @yield("content")
                 </div>
