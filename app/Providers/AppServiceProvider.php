e6ff48c7f7ba1e48e1fd975942b85a39eeb6662d
Specticall <josephyusmita@gmail.com>
2025-12-12 12:49:55 +0700

    feat: force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        URL::forceScheme('https');
+        if($this->app->environment("production")) {
+            URL::forceScheme('https');
+        }
     }
 
     /**
